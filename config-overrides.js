22f5fabf8de3e4b06bd6324f6d90eddd356bae8f
Mark <mark@remarkablemark.org>
2018-04-06 23:43:29 -0400

    chore(package): replace `react-scripts` with `react-app-rewired`

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+/**
+ * Config overrides.
+ *
+ * https://github.com/timarney/react-app-rewired#extended-configuration-options
+ */
+module.exports = (config, env) => {
+  // use default `create-react-app` config
+  return config;
+};
