4d39b6802a5287b7f9ad326d720bf90c5179eae0
Mark <mark@remarkablemark.org>
2019-11-16 17:26:56 -0500

    chore(scripts): tidy `deploy.sh` and update comments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# make sure working index is clean
+# ensure working index is clean
 stash_output=$(git add . && git stash)
 
-# run lint and tests first
-npm run lint && npm test
+# run lint
+npm run lint
 
-# remove `build` directory and build production bundle
-npm run clean && npm run build
+# run tests
+npm test
 
-# deploy contents in `build` to remote branch `gh-pages`
+# remove `build` folder
+npm run clean
+
+# build production artifacts
+npm run build
+
+# deploy `build` contents to remote branch `gh-pages`
 if [[ $(command -v npx) ]]; then
   npx --no-install gitploy build gh-pages
 else
   $(npm bin)/gitploy build gh-pages
 fi
 
-# pop saved stash (if applicable)
+# return working index to initial state (if applicable)
 if [[ $stash_output != 'No local changes to save' ]]; then
   git stash pop
 fi
