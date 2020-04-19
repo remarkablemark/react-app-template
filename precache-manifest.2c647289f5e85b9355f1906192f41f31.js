7a9bedb59ee0f03ca2ffb1f644bb341957eaf731
Mark <mark@remarkablemark.org>
2020-04-18 20:48:39 -0400

    gitploy

---
+++ precache-manifest.2c647289f5e85b9355f1906192f41f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec1ad3bf33f7fc50a64442f5ed7c96d",
+    "url": "/react-app-template/index.html"
+  },
+  {
+    "revision": "93e4580a07d6f158a7b3",
+    "url": "/react-app-template/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0df03b65362955a0fc5c",
+    "url": "/react-app-template/static/js/2.95e6f362.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-app-template/static/js/2.95e6f362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e4580a07d6f158a7b3",
+    "url": "/react-app-template/static/js/main.c1111672.chunk.js"
+  },
+  {
+    "revision": "5114166182f0a2ebb85d",
+    "url": "/react-app-template/static/js/runtime-main.a084cea9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
